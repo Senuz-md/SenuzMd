084f55386aef322b52153b832fdab30b47631e50
Senuz-md <Seneth049@gmail.com>
2025-10-04 23:42:00 +0530

    Update Session.js

--- Session.js
+++ Session.js
@@ -5,6 +5,6 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || "your-session-id",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628"],
     PORT: process.env.PORT || "8000",
-    SESSION_DIR: process.env.SESSION_DIR || "session",
+    SESSION_DIR: process.env.SESSION_DIR || "SENUZ-MD",
     FOOTER: process.env.FOOTER || "> SENUZ-MD | Developed by Senuz\n© 2025 SENUZ-MD. All rights reserved."
 };
